5510cd114ffe05801cda16356579120844a0957f
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-02-26 09:19:00 -0500

    Move the raml server into its own module.
    Update docker-compose.yml with more generic names.

--- app/index.ts
+++ app/index.ts
@@ -1,27 +1,8 @@
 /// <reference path="./typings/tsd.d.ts" />
-import * as express from "express";
-let _ = require("underscore");
-let raml2html = require("raml2html");
-let merge = require("merge");
+import {Server} from "./src/Server";
 
-let configWithDefaultTemplates = raml2html.getDefaultConfig();
+Server.app.listen(80, () => { console.log("Listening on 80"); });
 
-let render = (res: express.Response, path: string) => {
-  raml2html.render(path, configWithDefaultTemplates).then(
-    (result) => res.send(result),
-    (err: Error) => res.send(err)
-  );
-};
-
-let pickNonfalsy = _.partial(_.pick, _, _.identity);
-
-let app = express();
-app.get("/:file?", (req, res) => {
-  let params = merge({ file: "schema" }, pickNonfalsy(req.params));
-  render(res, `./${params.file}.raml`);
-});
-
-let server = app.listen(80, () => console.log("Listening on 80"));
 let exit = () => process.exit(0);
 process.on("SIGTERM", exit);
 process.on("SIGINT", exit);
---
+++ app/src/Server.ts
@@ -0,0 +1,24 @@
+/// <reference path="../typings/tsd.d.ts" />
+import * as express from "express";
+let _ = require("underscore");
+let raml2html = require("raml2html");
+let merge = require("merge");
+
+export namespace Server {
+  let configWithDefaultTemplates = raml2html.getDefaultConfig();
+
+  let render = (res: express.Response, path: string) => {
+    raml2html.render(path, configWithDefaultTemplates).then(
+      (result) => res.send(result),
+      (err: Error) => res.send(err)
+    );
+  };
+
+  let pickNonfalsy = _.partial(_.pick, _, _.identity);
+
+  export let app = express();
+  app.get("/:file?", (req, res) => {
+    let params = merge({ file: "schema" }, pickNonfalsy(req.params));
+    render(res, `./${params.file}.raml`);
+  });
+}
