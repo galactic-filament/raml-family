257f633f48d8cf37233ff1f0691f31ccaf77126a
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-02-26 09:24:51 -0500

    Tighten line spacing and add helpful comments.

--- app/src/Server.ts
+++ app/src/Server.ts
@@ -5,17 +5,17 @@ let raml2html = require("raml2html");
 let merge = require("merge");
 
 export namespace Server {
+  // misc
   let configWithDefaultTemplates = raml2html.getDefaultConfig();
-
   let render = (res: express.Response, path: string) => {
     raml2html.render(path, configWithDefaultTemplates).then(
       (result) => res.send(result),
       (err: Error) => res.send(err)
     );
   };
-
   let pickNonfalsy = _.partial(_.pick, _, _.identity);
 
+  // express server
   export let app = express();
   app.get("/:file?", (req, res) => {
     let params = merge({ file: "schema" }, pickNonfalsy(req.params));
